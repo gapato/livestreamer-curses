aad6e8e9b2325b7a775e4d29f4ae97a1f34601e3
Gapato <g@oknaj.eu>
2015-08-09 15:08:01 +0200

    [feature] use terminal's colors

--- src/livestreamer_curses/streamlist.py
+++ src/livestreamer_curses/streamlist.py
@@ -217,6 +217,11 @@ class StreamList(object):
     def init(self, s):
         """ Initialize the text interface """
 
+        if curses.can_change_color():
+            curses.use_default_colors()
+            for i in range(0, curses.COLORS):
+                curses.init_pair(i + 1, i, -1)
+
         # Hide cursor
         curses.curs_set(0)
 
